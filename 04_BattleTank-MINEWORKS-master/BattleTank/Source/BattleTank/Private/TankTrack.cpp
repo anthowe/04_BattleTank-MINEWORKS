f00d845eb6ea6d3cd4246eee8068b8aa300ade73
DESKTOP-J1PKA4L\ahowe <ahowe2@att.net>
2018-07-20 17:17:53 -0400

    After deleting starter content

---
+++ 04_BattleTank-MINEWORKS-master/BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankTrack.h"
+
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	
+}
+
+void UTankTrack::BeginPlay()
+{
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) 
+{
+	ApplySidewaysForce();
+	DriveTrack();
+	CurrentThrottle = 0;
+}
+
+void UTankTrack::ApplySidewaysForce()
+{
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	auto DeltaTime = GetWorld()->GetDeltaSeconds();
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = TankRoot->GetMass() * CorrectionAcceleration / 2;
+	TankRoot->AddForce(CorrectionForce);
+}
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
+}
+
+void UTankTrack::DriveTrack()
+{
+	auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
---
+++ 04_BattleTank-MINEWORKS-master/BattleTank/Source/BattleTank/Public/Tank.h
@@ -0,0 +1,19 @@
+// Copyright EmbraceIT Ltd.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "Tank.generated.h" // Put new includes above
+
+
+
+UCLASS()
+class BATTLETANK_API ATank : public APawn
+{
+	GENERATED_BODY()
+
+		ATank();
+
+
+};
+
